f13a6c413f9a59a7e5f33b8789e2510e9c43febb
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2022-12-27 10:38:54 +0600

    adding 2 new feature, wiki & playing

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 # import section
 import pyttsx3
 import speech_recognition as sr
+import pywhatkit
+import wikipedia
+
+
+def wiki(command):
+    command = wikipedia.summary(command, sentences=2)
+    talk(command)
 
 
 # A function for next work!
-
-
 def talk(text):
     m = pyttsx3.init()
     m.say(text)
@@ -13,17 +18,22 @@ def talk(text):
 
 
 listener = sr.Recognizer()
-while True:
 
-    try:
-        with sr.Microphone() as source:
-            print("Listening...")
-            voice = listener.listen(source)
+try:
+    with sr.Microphone() as source:
+        print("Listening...")
+        voice = listener.listen(source)
 
-            command = listener.recognize_google(voice)
+        command = listener.recognize_google(voice)
+        if 'play' in command:
+            command = command.replace('play', '')
+            print('opening youtube and play ' + command)
+            pywhatkit.playonyt(command)
+        elif 'tell' in command:
+            print('telling about ' + command)
+            command = command.replace('tell', '')
+            wiki(command)
 
-            print(command)
-            talk(command)
 
-    except:
-        talk('please say something! ')
+except:
+    talk('please say something!')
