1ff3012e9b96af70ebfd5930906ae7f72f5c7858
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2022-12-27 08:58:49 +0600

    adding respons function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # import section
 import pyttsx3
+import speech_recognition as sr
+
 
 # A function for next work!
 
@@ -8,3 +10,20 @@ def talk(text):
     m = pyttsx3.init()
     m.say(text)
     m.runAndWait()
+
+
+listener = sr.Recognizer()
+while True:
+
+    try:
+        with sr.Microphone() as source:
+            print("Listening...")
+            voice = listener.listen(source)
+
+            command = listener.recognize_google(voice)
+
+            print(command)
+            talk(command)
+
+    except:
+        talk('please say something! ')
